52ed64a8c2f6c38a7bb32e4858573c5c1aed12a8
dempseydeclerck <info@zoobli.io>
2022-09-01 22:46:31 +0200

    Create vendor composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,222 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '63a67d81fd2661ae520d20d9bc6813bc0cadd0d3',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '63a67d81fd2661ae520d20d9bc6813bc0cadd0d3',
+    ),
+    'composer/ca-bundle' => 
+    array (
+      'pretty_version' => '1.2.10',
+      'version' => '1.2.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9fdb22c2e97a614657716178093cd1da90a64aa8',
+    ),
+    'embed/embed' => 
+    array (
+      'pretty_version' => '3.4.17',
+      'version' => '3.4.17.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2c0e112f7332597ec6a55174f2353e04859ba356',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.2.0',
+      'version' => '5.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'feb0e820b8436873675fd3aca04f3728eb2185cb',
+    ),
+    'hybridauth/hybridauth' => 
+    array (
+      'pretty_version' => '3.6.0',
+      'version' => '3.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '222ab4e6ee6ffd81caa77283142f3aa97afa5863',
+    ),
+    'mofodojodino/profanity-filter' => 
+    array (
+      'pretty_version' => '1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '529d24adcdb5b9507a4622db6aba6e54c5bb82a9',
+    ),
+    'nezamy/route' => 
+    array (
+      'pretty_version' => 'v1.2.6',
+      'version' => '1.2.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a5050b028ee39b383a949058a35baa284f396a0',
+    ),
+    'nezamy/support' => 
+    array (
+      'pretty_version' => 'v1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8803292708fbfa857bdd056c9b9dc883adc51414',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.1.7',
+      'version' => '6.1.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2c2370ba3df7034f9eb7b8f387c97b52b2ba5ad0',
+    ),
+    'phpseclib/phpseclib' => 
+    array (
+      'pretty_version' => '2.0.30',
+      'version' => '2.0.30.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '136b9ca7eebef78be14abf90d65c5e57b6bc5d36',
+    ),
+    'plasticbrain/php-flash-messages' => 
+    array (
+      'pretty_version' => 'v1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b565762619342057ce561de20d0f2f6e14f9aa77',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.14.0',
+      'version' => '1.14.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fbdeaec0df06cf3d51c93de80c7eb76e271f5a38',
+    ),
+    'symfony/polyfill-iconv' => 
+    array (
+      'pretty_version' => 'v1.18.1',
+      'version' => '1.18.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6c2f78eb8f5ab8eaea98f6d414a5915f2e0fce36',
+    ),
+    'symfony/polyfill-intl-grapheme' => 
+    array (
+      'pretty_version' => 'v1.18.1',
+      'version' => '1.18.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b740103edbdcc39602239ee8860f0f45a8eb9aa5',
+    ),
+    'symfony/polyfill-intl-normalizer' => 
+    array (
+      'pretty_version' => 'v1.18.1',
+      'version' => '1.18.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '37078a8dd4a2a1e9ab0231af7c6cb671b2ed5a7e',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.14.0',
+      'version' => '1.14.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '34094cfa9abe1f0f14f48f490772db7a775559f2',
+    ),
+    'symfony/polyfill-php72' => 
+    array (
+      'pretty_version' => 'v1.18.1',
+      'version' => '1.18.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '639447d008615574653fb3bc60d1986d7172eaae',
+    ),
+    'thingengineer/mysqli-database-class' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9a95f2488495f13494e7c19ba7ad452946e947a7',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v2.12.5',
+      'version' => '2.12.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '18772e0190734944277ee97a02a9a6c6555fcd94',
+    ),
+    'vlucas/valitron' => 
+    array (
+      'pretty_version' => 'v1.4.7',
+      'version' => '1.4.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a03072bed1679660200766d134507a30193eaf6d',
+    ),
+    'voku/anti-xss' => 
+    array (
+      'pretty_version' => '4.1.28',
+      'version' => '4.1.28.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8919a19846d2b100e664d9e3cecfcaba4f57d9b4',
+    ),
+    'voku/portable-ascii' => 
+    array (
+      'pretty_version' => '1.5.3',
+      'version' => '1.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '25bcbf01678930251fd572891447d9e318a6e2b8',
+    ),
+    'voku/portable-utf8' => 
+    array (
+      'pretty_version' => '5.4.47',
+      'version' => '5.4.47.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c92522515a2d5ec9b03fd3c9e231b8b277c65121',
+    ),
+  ),
+);
