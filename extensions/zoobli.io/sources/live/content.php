1cc25397a35f6d6a3c7cc6e582b24b4ca011757f
dempseydeclerck <info@zoobli.io>
2022-09-02 21:36:30 +0200

    Create content.php

---
+++ extensions/zoobli.io/sources/live/content.php
@@ -0,0 +1,18 @@
+<?php
+
+if (IS_LOGGED == false) {
+    header("Location: " . PT_Link('login'));
+    exit();
+}
+
+$pt->page_url_ = $pt->config->site_url.'/live/';
+$pt->page        = 'live';
+$pt->title       = $lang->live . ' | ' . $pt->config->title;
+$pt->description = $pt->config->description;
+$pt->keyword     = $pt->config->keyword;
+$pt->content     = PT_LoadPage('live/content', array(
+    'LIVE_URL' => $pt->config->live_url,
+    'LIVE_URL_BASE64' => base64_encode($pt->config->live_url),
+    'USERNAME' => $pt->user->username
+
+));
